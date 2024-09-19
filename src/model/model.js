8a64e82b3789f37c81e7c8c81e632bb4a3ba1ecb
Tran Dang Duat <23020023@vnu.edu.vn>
2024-09-19 23:32:23 +0700

    Evaluate all possible strings created from predictions of all the expressions

--- src/model/model.js
+++ src/model/model.js
@@ -1,8 +1,10 @@
 import * as tf from '@tensorflow/tfjs';
 import { extractExpressions, preprocessImage } from './imageProcessing';
+import Mexp from 'math-expression-evaluator';
+const mexp = new Mexp();
 
 const MODEL_PATH = 'recognizer_model/model.json';
-const MathExpr = [ '!', '(', ')', '+', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '=', 'X', 'cos', 'div', 'i', 'j', 'k', 'log', 'pi', 'sin', 'sqrt', 'tan', 'times', 'u', 'v', 'y', 'z' ];
+const MathExpr = [ '!', '(', ')', '+', '-', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '=', 'X', 'cos', '/', 'i', 'j', 'k', 'log', 'pi', 'sin', 'sqrt', 'tan', '*', 'u', 'v', 'y', 'z' ];
 
 let model = null;
 
@@ -17,21 +19,51 @@ export async function predict(imageData) {
     }
     const img = await preprocessImage(imageData);
     const prediction = model.predict(img).dataSync();
-    const result = MathExpr[prediction.indexOf(Math.max(...prediction))];
-    // for (let i = 0; i < prediction.length; i++) {
-    //     console.log(MathExpr[i], ":", Math.round(prediction[i] * 100) + "%");
-    // }
-    // console.log("Prediction: ", result);
-    return result;
+    const results = [];
+    for (let i = 0; i < prediction.length; i++) {
+        if (prediction[i] >= 0.05) {
+            results.push({
+                expression: MathExpr[i],
+                confidence: prediction[i]
+            });
+        }
+    }
+    return results;
+}
+
+function getAllCombinations(expressions, currentString, currentConfidence, results) {
+    let index = currentString.length;
+    if (index === expressions.length) {
+        results.push({
+            expression: currentString,
+            confidence: currentConfidence,
+            evalResult: (() => {
+                try {
+                    return mexp.eval(currentString);
+                } catch (e) {
+                    return NaN;
+                }
+            })()
+        });
+        return;
+    }
+    expressions[index].forEach((expr) => {
+        getAllCombinations(expressions, currentString + expr.expression, currentConfidence * expr.confidence, results);
+    });
 }
 
 export async function predictExpressions(imageData) {
     const expressionImages = await extractExpressions(imageData);
     let T = performance.now();
-    let expressions = "";
+
+    let expressions = [];
     for (let imgData of expressionImages) {
-        expressions += await predict(imgData);
+        expressions.push(await predict(imgData));
     }
+    console.log(expressions);
+    let results = [];
+    getAllCombinations(expressions, "", 1, results);
+
     console.log("Time taken: ", performance.now() - T);
-    return expressions;
+    return results;
 }
